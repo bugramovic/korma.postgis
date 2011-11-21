a9b8f18e506eee75323e810c61a5d8cf909ead3f
bugramovic <bugramovic@googlemail.com>
2011-11-22 00:00:43 +0100

    currently depends on my korma fork

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
-(defproject korma_postgis "1.0.0-SNAPSHOT"
+(defproject korma_postgis "0.0.1"
   :description "some conviance for workting with korma and postgis databases"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [korma "0.2.2-SNAPSHOT"]]
+                [korma "0.3.0-alpha3-ab"]
+                 [com.vividsolutions/jts "1.12"]
+                 [org.postgis/postgis-jdbc "1.3.3"
+                    :exclusions [postgresql/postgresql] ]]
    
   :autodoc {:name "Korma Postigs" :page-title "Korma Postgis Docs"}  
   :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]
-		     [enlive "1.0.0"]
+		                 [enlive "1.0.0"] ;for the documentation scraper
                      [postgresql/postgresql "9.0-801.jdbc4"]]
-                     
 )
