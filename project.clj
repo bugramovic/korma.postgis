cdab7188ddacdead7a21e542b1bb0038399671f6
Vlad Bokov <razum2um@mail.ru>
2014-09-22 09:30:24 +0700

    bump versions

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
 (defproject korma_postgis "0.0.1"
   :description "some conviance for workting with korma and postgis databases"
   :java-source-path "src"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                ;[korma "0.3.0-alpha3-ab"]
-                 [korma "0.3.0-alpha4"]
-                 [com.vividsolutions/jts "1.12"]
+  :dependencies [[korma "0.4.0"]
+                 [com.vividsolutions/jts "1.13"]
                  [org.postgis/postgis-jdbc "1.3.3"
                     :exclusions [postgresql/postgresql] ]]
    
